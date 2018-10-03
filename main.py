d94c46d3aaa9ebdab1b06b50837a9adc3b4a7311
PhenoloBoy <pl.marasco@gmail.com>
2018-10-03 09:14:06 +0200

    logging added

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import time
+import logging
+import webbrowser
 
 
 def cleaner(pixel, **kwargs):
@@ -18,23 +20,28 @@ def cleaner(pixel, **kwargs):
 
 def main():
 
+    logging.basicConfig(level=10)
     times = pd.date_range('2000-01-01', periods=100)
     x = range(30)
     y = range(30)
     cube = xr.DataArray(np.random.rand(len(times), len(x), len(y)), coords=[times, x, y], dims=['time', 'x', 'y'])
+    logging.info('Cube created')
 
     x = int(cube.sizes['x'])
     y = int(cube.sizes['y'])
 
     pixels_pairs = np.indices((x, y)).transpose((1, 2, 0)).reshape((x * y, 2))
+    logging.info('Pairs created')
 
     client = Client()
-    print('Client ready')
+    logging.info('Client ready')
 
-    futures = client.map(cleaner, pixels_pairs, cube=cube)
-    print('End future creation')
+    cubes = client.scatter(cube)
+    logging.info('Scattered')
 
-    import webbrowser
+    futures = client.map(cleaner, pixels_pairs, cube=cubes)
+    logging.info('Futures ready')
+
     url = 'http://localhost:8787/status'
     webbrowser.open_new(url)
 
@@ -43,8 +50,11 @@ def main():
         cube[dict([('x', pixel[0]), ('y', pixel[1])])] = ts
 
     print(cube)
-    print('Done!')
+    logging.info('Done!')
 
     client.close()
 
 
+if __name__ == '__main__':
+    logging.info('Entrato')
+    main()
