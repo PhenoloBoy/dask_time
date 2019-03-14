e1aa7edfdbccac8dc60235a88f0a6ab6e51b6e90
pl.marasco@gmail.com <pl.marasco@gmail.com>
2019-03-14 08:13:50 +0100

    .values to get the numbers

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def function(chunk, **kwargs):
     rowi = kwargs.pop('data', '')
     parameter = kwargs.pop('parameter', '')
 
-    chunk_out = pd.DataFrame(index=rowi.time, columns=pd.DataFrame(chunk)[1])
+    chunk_out = pd.DataFrame(index=rowi.time.values, columns=pd.DataFrame(chunk)[1])
 
     try:
         for i in chunk:
@@ -36,15 +36,15 @@ def function(chunk, **kwargs):
 def main():
 
     # Data creation
-    times = pd.date_range('2000-01-01', periods=800) # to stress more the system just increase the period value
+    times = pd.date_range('2000-01-01', periods=300) # to stress more the system just increase the period value
     x = range(1)
-    y = range(int(14e4))
+    y = range(int(14e3))
     cube = xr.DataArray(np.random.rand(len(times), len(x), len(y)), coords=[times, x, y], dims=['time', 'x', 'y'])
     pixels_pairs = np.argwhere(cube.isel(time=0).values)
 
     # Client
-    # client = Client(processes=False, n_workers=1, threads_per_worker=1)
-    client = Client(n_workers=6)
+    client = Client(processes=False, n_workers=1, threads_per_worker=1)
+    # client = Client()
 
     url = 'http://localhost:8787/status'
     webbrowser.open_new(url)
@@ -53,7 +53,7 @@ def main():
         row = cube.isel(dict([('x', row_idx)]))
 
         px_list = [ith for ith in pixels_pairs if ith[0] == row_idx]
-        output_carrier = pd.DataFrame(index=cube.time, columns=cube.y)
+        output_carrier = pd.DataFrame(index=cube.time.values, columns=cube.y.values)
 
         chunks = np.array_split(px_list, multiprocessing.cpu_count()*4)
         rowi = client.scatter(row, broadcast=True)
